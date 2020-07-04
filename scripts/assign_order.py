9df43915a7eb247f63d278da05077a23246b685f
Brendan Doney <bre.doney@gmail.com>
2020-07-04 12:39:06 -0400

    Use order ascension as discriminator in assign_order script

--- scripts/assign_order.py
+++ scripts/assign_order.py
@@ -3,48 +3,70 @@
 import os
 import re
 import fileinput
+from typing import Dict, List
 
 start_dir = "./_docs/java/"
-
 type_regex = re.compile(r"type: (\w+)")
-order_regex = re.compile(r"order: \w+")
-suggested_order = 1
+order_regex = re.compile(r"order: (\w+)")
 
 
-def get_type_map(root, files):
-    type_map = {"lesson": [], "exercise": []}
-    for file in files:
-        path = os.path.join(root, file)
+class ParsedFile:
+
+    def __init__(self, path: str) -> None:
+        self.path = path
+        self._current_order = None
+        self.doc_type = None
 
         with open(path, "r") as f:
             for line in f:
-                match = type_regex.match(line)
-                if match is not None:
-                    file_type = match.group(1)
-                    if file_type in type_map:
-                        type_map[file_type].append(path)
-                        break
+                if self._current_order is None:
+                    match = order_regex.match(line)
+                    if match is not None:
+                        self._current_order = int(match.group(1))
+                elif self.doc_type is None:
+                    match = type_regex.match(line)
+                    if match is not None:
+                        self.doc_type = match.group(1)
+
+    def get_order(self):
+        return self._current_order
+
+    def set_order(self, order: int):
+        matched = False
+        f = fileinput.input(self.path, inplace=True)
+        for line in f:
+            # Prints here are directed to the output file, not terminal stdout
+            if not matched and order_regex.match(line) is not None:
+                print(f"order: {order}")
+                matched = True
+            else:
+                print(line, end="")
+
+        self._current_order = order
+
+    def __repr__(self) -> str:
+        return f"'{self.path}' {self._current_order} - {self.doc_type}"
+
+
+def get_type_map(parsed_files: List[ParsedFile]) -> Dict[str, List[ParsedFile]]:
+    type_map = {"lesson": [], "exercise": []}
+    for file in parsed_files:
+        t = file.doc_type
+        if t is not None:
+            type_map[t].append(file)
     return type_map
 
 
-def change_order(documents):
-    global suggested_order
-
-    if len(documents) > 1:
-        for path in documents:
-            order = prompt_order_change(path, suggested_order)
-            write_order_change(path, order)
-            suggested_order += 1
-    elif len(documents) == 1:
-        path = documents[0]
-        print(f"{path} order -> {suggested_order}")
-        write_order_change(path, suggested_order)
-        suggested_order += 1
+def order_is_ascending(starting_order: int,
+                       parsed_files: List[ParsedFile]) -> bool:
+    orders = {file.get_order() for file in parsed_files}
+    expected = set(range(starting_order, starting_order + len(parsed_files)))
+    return orders == expected
 
 
-def prompt_order_change(path, suggested_order):
+def prompt_order_change(parsed_file: ParsedFile, suggested_order: int) -> int:
     try:
-        new_order = input(f"{path} -> {suggested_order} ? ")
+        new_order = input(f"{parsed_file.path} -> {suggested_order} ? ")
         if len(new_order) > 0:
             return int(new_order)
     except EOFError:
@@ -53,35 +75,64 @@ def prompt_order_change(path, suggested_order):
     return suggested_order
 
 
-def write_order_change(path, order):
-    matched = False
-    f = fileinput.input(path, inplace=True)
-    for line in f:
-        # Prints here are directed to the output file, not terminal stdout
-        if not matched and order_regex.match(line) is not None:
-            print(f"order: {order}")
-            matched = True
+def fix_order(starting_order: int, parsed_files: List[ParsedFile]) -> int:
+    properly_ordered = order_is_ascending(starting_order, parsed_files)
+    suggested_order = starting_order
+
+    for file in parsed_files:
+        if not properly_ordered:
+            new_order = prompt_order_change(file, suggested_order)
+            file.set_order(new_order)
         else:
-            print(line, end="")
+            print(f"{file.path} -> {suggested_order}")
+        suggested_order += 1
+
+    return suggested_order
 
 
-sorted_dirs = False
-for root, _dirs, files in os.walk(start_dir):
-    if not sorted_dirs:
-        # Runs only on the first iteration, which is when looking through start_dir
-        _dirs.sort()
-        sorted_dirs = True
+def fix_order_by_category(starting_order: int,
+                          parsed_files: List[ParsedFile]) -> int:
+    """
+    Fix the order of the parsed files, separating by category so that lessons are corrected before
+    exercises.
 
-    type_map = get_type_map(root, files)
+    :param starting_order: the expected order of the first parsed file
+    :type starting_order: int
+    :param parsed_files: the files to correct the order of
+    :type parsed_files: List[ParsedFile]
+    :return: the suggested order of the last parsed file
+    :rtype: int
+    """
+
+    type_map = get_type_map(parsed_files)
+    suggested_order = starting_order
 
     lessons = type_map["lesson"]
     if len(lessons) > 0:
         print("Lessons:")
-        change_order(lessons)
+        suggested_order = fix_order(suggested_order, lessons)
 
     exercises = type_map["exercise"]
     if len(exercises) > 0:
         print("Exercises:")
-        change_order(exercises)
+        suggested_order = fix_order(suggested_order, exercises)
 
     print()
+
+    return suggested_order
+
+
+suggested_order = 1
+sorted_dirs = False
+for root, _dirs, files in os.walk(start_dir):
+    if not sorted_dirs:
+        # Runs only on the first iteration (when looking through start_dir) so no files are skipped
+        _dirs.sort()
+        sorted_dirs = True
+
+    parsed_files = []
+    for file in files:
+        full_path = os.path.join(root, file)
+        parsed_files.append(ParsedFile(full_path))
+
+    suggested_order = fix_order_by_category(suggested_order, parsed_files)
